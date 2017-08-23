5a8f86ca06b4d4f7322c4b732583fcfd11c2ff23
Sulejman <sulejmansarajlija@gmail.com>
2017-08-23 03:02:24 +0200

    test

---
+++ test/VotingDapp.test.js
@@ -0,0 +1,12 @@
+var VotingDapp = artifacts.require('./VotingDapp/VotingDapp.sol');
+
+contract('VotingDapp', function(accounts) {
+  it("should create poll available for anyone", function() {
+    var voting;
+
+    // Get initial balances of starting accounts.
+    var account1 = accounts[0];
+
+    
+  });
+});
